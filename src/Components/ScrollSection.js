44dbe8a84d1990942b78ef2a22ba41ca708147a3
johntettis <johndavidtettis@gmail.com>
2024-11-15 16:09:34 -0500

    added scrollbar reference to allow global scrolling

--- src/Components/ScrollSection.js
+++ src/Components/ScrollSection.js
@@ -1,52 +1,21 @@
-import Bio from './Bio';
-import React, { useEffect, useRef } from 'react';
+import Description from './Description';
+import WorkHistory from './WorkHistory';
+import React, { forwardRef, useEffect, useRef } from 'react';
 import '../styles/scrollSection.css'
  
-const ScrollSection= ({children})=>{
+const ScrollSection= forwardRef((props,ref)=>{
 
 
     return(
-    <div className='scroll-section'>
-        <p className="fade-in">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
-         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
-          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
-           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
-        </p>
-
-        <p className="fade-in">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
-         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
-          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
-           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
-        </p>
-
-
-
-        <p className="fade-in">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
-         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
-          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
-           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
-        </p>
-
-
-        <p className="fade-in">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
-         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
-          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
-           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
-        </p>
-
+    <div ref={ref}className='scroll-section'>
+        <Description/>
+        <WorkHistory/>
+        
     </div>
         
 
     )
 
-}
+});
 
 export default ScrollSection
