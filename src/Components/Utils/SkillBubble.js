a4f14d24c4344f2a39712d3b827113fb93ccbd5a
johntettis <johndavidtettis@gmail.com>
2024-11-15 16:15:00 -0500

    added skill bubble for job description

---
+++ src/Components/Utils/SkillBubble.js
@@ -0,0 +1,16 @@
+import '../../styles/skillBubble.css'
+
+
+
+const SkillBubble = ({skill})=>{
+
+
+    return(
+    <>
+     <span className = "skill-bubble">{skill}</span>
+    
+    </>
+
+    )
+}
+export default SkillBubble
