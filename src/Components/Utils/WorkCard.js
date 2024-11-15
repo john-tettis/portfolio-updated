c38770db125dee9ac5ef3fb7531e46f4c363180b
johntettis <johndavidtettis@gmail.com>
2024-11-15 16:10:50 -0500

    added duration and skills sections

--- src/Components/Utils/WorkCard.js
+++ src/Components/Utils/WorkCard.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useRef } from 'react';
-// import SkillBubble from './SkillBubble'
+import SkillBubble from './SkillBubble'
 import '../../styles/Card.css'
 
-const WorkCard= ({title, company, description, skills})=>{
+const WorkCard= ({title, company, description, duration,skills})=>{
 
 
     return(
     <div className='card'>
-        <h4 className='card-title'>{title} - {company}</h4>
-        <p className="card-description">{description}</p>
-        <div className="card-skills"></div>
-        
+        <div className="card-column-left">
+            <h4>{duration}</h4>
+            <div className="card-skills">
+                {skills.map((skill, index) => (
+                    <SkillBubble 
+                        key={index} 
+                        skill={skill}
+                    />
+                ))}
+            </div>
+        </div>
+         <div className="card-column-right">
+            <h4 className='card-title'>{title}</h4>
+            <p className="card-company">{company}</p>
+            <p className="card-description">{description}</p>
+            
+
+        </div>
     </div>
         
 
