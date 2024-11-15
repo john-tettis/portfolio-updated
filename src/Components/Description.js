94e4d0bfa8201032a7a9e7ae70a898747c742b19
johntettis <johndavidtettis@gmail.com>
2024-11-15 16:14:39 -0500

    moved description text into seperate file

---
+++ src/Components/Description.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from 'react';
+import '../styles/description.css'
+ 
+const Description= ({children})=>{
+
+
+    return(
+    <div className='description'>
+        <p className="fade-in">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
+         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
+          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
+           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
+        </p>
+
+        <p className="fade-in">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
+         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
+          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
+           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
+        </p>
+
+
+
+        <p className="fade-in">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
+         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
+          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
+           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
+        </p>
+
+
+        <p className="fade-in">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
+         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
+          Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
+           tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
+        </p>
+
+    </div>
+        
+
+    )
+
+}
+
+export default Description
