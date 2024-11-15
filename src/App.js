54f9c750319bf21df5b256984dd2f31f2e8392f8
johntettis <johndavidtettis@gmail.com>
2024-11-15 16:11:55 -0500

    added scrollbar reference to allow global scrolling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './styles/App.css';
 import DynamicBackground from './Components/DynamicBackground';
 import BodyContent from './Components/BodyContent'
 
 function App() {
+  const scrollableSectionRef = useRef(null);
+
+  useEffect(() => {
+    const handleWheel = (event) => {
+      if (scrollableSectionRef.current) {
+        scrollableSectionRef.current.scrollTop += event.deltaY; // Adjust scroll based on mouse wheel movement
+        console.log(event)
+      }
+    };
+
+    // Listen for mouse wheel events on the body
+    document.body.addEventListener('wheel', handleWheel);
+
+    // Clean up event listener on component unmount
+    return () => {
+      document.body.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
   return (
     <div className="App">
       <DynamicBackground/>
-      <BodyContent/>
+      <BodyContent
+      scrollRef = {scrollableSectionRef}
+      />
     </div>
   );
 }
+
 
 export default App;
