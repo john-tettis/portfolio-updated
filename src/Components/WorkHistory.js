c4c4fcfffcc4e00248772abaebd184d8757b1176
johntettis <johndavidtettis@gmail.com>
2024-11-15 16:11:28 -0500

    updated placeholder data

--- src/Components/WorkHistory.js
+++ src/Components/WorkHistory.js
@@ -6,17 +6,59 @@ import '../styles/workHistory.css'
 
 const history = [
     {
-        title:"WORK I DID",
+        title:"Language Model Engineer",
         company:"DATA ANNOTATION",
-        description:```
+        description:`
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
         convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
         Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
         tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
-        ```
-
+        `,
+        duration:"2024 - Present",
+        skills:["Python", "Flask","JavaScript", "Express","SQL", "Data Analytics"]
+    },
+    {
+        title:"Language Model Engineer",
+        company:"DATA ANNOTATION",
+        description:`
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
+        convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
+        Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
+        tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
+        `,
+        duration:"2024 - Present",
+        skills:["Python", "Flask","JavaScript", "Express","SQL", "Data Analytics"]
+    },
+    {
+        title:"Language Model Engineer",
+        company:"DATA ANNOTATION",
+        description:`
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
+        convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
+        Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
+        tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
+        `,
+        duration:"2024 - Present",
+        skills:["Python", "Flask","JavaScript", "Express","SQL", "Data Analytics"]
+    },
+    {
+        title:"Language Model Engineer",
+        company:"DATA ANNOTATION",
+        description:`
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Cras neque dui, interdum tempus lacus ac, iaculis pharetra lorem. Sed ultricies dolor quis elit 
+        convallis accumsan. Suspendisse sapien felis, volutpat quis purus nec, consectetur condimentum orci.
+        Nunc sed risus diam. Sed viverra elit sit amet iaculis maximus. Proin euismod semper risus. Aliquam semper
+        tincidunt nisl, ut feugiat lectus fringilla at. Duis laoreet dui semper porttitor semper. Nulla facilisi. 
+        `,
+        duration:"2024 - Present",
+        skills:["Python", "Flask","JavaScript", "Express","SQL", "Data Analytics"]
     }
+    
+
 ]
 const WorkHistory= ()=>{
 
@@ -24,16 +66,15 @@ const WorkHistory= ()=>{
     return(
     <div className='work-history'>
         {history.map((job, index) => (
-          <Card 
+          <WorkCard 
             key={index} 
+            duration={job.duration}
             title={job.title} 
             company={job.company} 
             description={job.description} 
+            skills={job.skills}
           />
         ))}
-        
-
-        
     </div>
         
 
