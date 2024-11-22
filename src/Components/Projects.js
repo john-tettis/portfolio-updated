0f3967dd1ea0b2f9f84bc3ad6158883e2a5e9b31
johntettis <johndavidtettis@gmail.com>
2024-11-22 17:50:05 -0500

    added refs for nav links
    added some placeholder copies to test spacing

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {forwardRef} from 'react';
 import ProjectCard from './Utils/ProjectCard'
 import '../styles/workHistory.css'
 import raspberryPI from '../Assets/Images/raspberry-pi.png'
@@ -13,13 +13,37 @@ const projects = [
         `,
         imgUrl:raspberryPI,
         skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
+    },
+    {
+        title:"Raspberry PI Video Server",
+        link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
+        description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
+        `,
+        imgUrl:raspberryPI,
+        skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
+    },
+    {
+        title:"Raspberry PI Video Server",
+        link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
+        description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
+        `,
+        imgUrl:raspberryPI,
+        skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
+    },
+    {
+      title:"Raspberry PI Video Server",
+      link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
+      description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
+      `,
+      imgUrl:raspberryPI,
+      skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
     }
 ]
-const Projects = (props,ref)=>{
+const Projects =  forwardRef((props,ref)=>{
 
 
     return(
-    <section id='projects'>
+    <section ref={ref} id='projects'>
         {projects.map((p, index) => (
           <ProjectCard 
             key={index} 
@@ -35,6 +59,6 @@ const Projects = (props,ref)=>{
 
     )
 
-}
+})
 
 export default Projects
