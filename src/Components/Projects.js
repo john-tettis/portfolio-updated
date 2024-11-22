e79af37903b2535365c28f5c03a596a10c2ffa7f
johntettis <johndavidtettis@gmail.com>
2024-11-22 17:10:32 -0500

    project structure

---
+++ src/Components/Projects.js
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import ProjectCard from './Utils/ProjectCard'
+import '../styles/workHistory.css'
+import raspberryPI from '../Assets/Images/raspberry-pi.png'
+
+
+const projects = [
+    {
+        title:"Raspberry PI Video Server",
+        link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
+        description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
+        `,
+        imgUrl:raspberryPI,
+        skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
+    }
+]
+const Projects = (props,ref)=>{
+
+
+    return(
+    <section id='projects'>
+        {projects.map((p, index) => (
+          <ProjectCard 
+            key={index} 
+            link={p.link}
+            title={p.title} 
+            description={p.description} 
+            imgUrl={p.imgUrl}
+            skills={p.skills}
+          />
+        ))}
+    </section>
+        
+
+    )
+
+}
+
+export default Projects
