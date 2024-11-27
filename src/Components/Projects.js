f819e163feeff127c3d760213d3ab35bbb8788e9
John Tettis <johndavidtettis@gmail.com>
2024-11-27 15:32:46 -0500

    updated copy

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,40 +3,31 @@ import React, {forwardRef} from 'react';
 import ProjectCard from './Utils/ProjectCard'
 import '../styles/WorkHistory.css'
 import raspberryPI from '../Assets/Images/raspberry-pi.png'
+import dat from '../Assets/Images/DAT.jpg'
 
 
 const projects = [
     {
         title:"Raspberry PI Video Server",
         link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
-        description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
+        description:`Remote video server run on a Raspberry PI. 
+        Receives Youtube video link from user's device, runs selenium, starts projector, and displays said 
+        video over the projector. Features include favorited youtube videos, audio control, and more!
         `,
         imgUrl:raspberryPI,
-        skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
+        skills:["Flask", "Selenium","Static Web Serving","SQL", "Data Analytics"]
     },
     {
-        title:"Raspberry PI Video Server",
-        link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
-        description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
+        title:"Data Annotation Chrome Extension",
+        link:"https://chromewebstore.google.com/detail/dat-extension/olnfeojadebhopefbogmjdpekifcdmkp",
+        repo:"",
+        description:`
+        Chrome extension myself and two others developed for general use on our company's website. 
+        The goal and acheivement of this extension was and is to streamline company procedure and increase productivity across the board.
+        Currently has 60 active users.
         `,
-        imgUrl:raspberryPI,
-        skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
-    },
-    {
-        title:"Raspberry PI Video Server",
-        link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
-        description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
-        `,
-        imgUrl:raspberryPI,
-        skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
-    },
-    {
-      title:"Raspberry PI Video Server",
-      link:"https://github.com/john-tettis/RaspberryPi-remote-video-server",
-      description:`Remote video server for a raspberry pi to stream youtube videos to a projector blah blah blah blah blah blah blah blah
-      `,
-      imgUrl:raspberryPI,
-      skills:["Flask","Node.js", "Express","React","SQL", "Data Analytics"]
+        imgUrl:dat,
+        skills:["JavaScript", "HTML","Chrome API"]
     }
 ]
 const Projects =  forwardRef((props,ref)=>{
