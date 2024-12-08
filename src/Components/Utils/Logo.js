979b7e4113a1b0e9f8857514ff5695d4982899e3
John Tettis <johndavidtettis@gmail.com>
2024-12-07 19:24:30 -0500

    updated logo - stroke weight and color removed for css access

--- src/Components/Utils/Logo.js
+++ src/Components/Utils/Logo.js
@@ -1,253 +1,80 @@
 import '../../styles/Logo.css'
 
-// export default function Logo() {
-//     return (
-//       <svg
-//         width="210mm"
-//         height="56mm"
-//         viewBox="0 0 210 56"
-//         version="1.1"
-//         id="svg1"
-//         xmlns="http://www.w3.org/2000/svg"
-//         xmlnsXlink="http://www.w3.org/1999/xlink"
-//       >
-//         <defs id="defs1" />
-//         <g id="layer1">
-//           <g
-//             className="logo"
-//             style={{
-//               fill: "none",
-//               stroke: "#000000",
-//               strokeLinecap: "round",
-//               strokeLinejoin: "round",
-//             }}
-//             transform="scale(0.264583)"
-//             id="g12"
-//           >
-//             <g className="logo" transform="translate(103.25,90.9338)" id="g11">
-//               <path
-//                 d="M 472,662 V 158 L 441,63 410,31.5 346,0 H 284 L 220,31.5 189,63 158,158 v 62"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="scale(0.064,-0.064)"
-//                 id="path2"
-//               />
-//               <path
-//                 d="M 346,441 284,410 220,346 189,252 V 189 L 220,94.5 284,31.5 346,0 h 95 l 63,31.5 63,63 31,94.5 v 63 l -31,94 -63,64 -63,31 h -95"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,32.256,0)"
-//                 id="path3"
-//               />
-//               <path
-//                 d="M 220,662 V 0 m 0,315 95,95 63,31 h 94 l 64,-31 31,-95 V 0"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,70.528,0)"
-//                 id="path4"
-//               />
-//               <path
-//                 d="M 220,441 V 0 m 0,315 95,95 63,31 h 94 l 64,-31 31,-95 V 0"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,108.8,0)"
-//                 id="path5"
-//               />
-//               <path
-//                 d="M 346,662 V 0 M 126,662 h 441"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,171.264,0)"
-//                 id="path6"
-//               />
-//               <path
-//                 d="m 189,252 h 378 v 63 l -31,63 -32,32 -63,31 H 346 L 284,410 220,346 189,252 V 189 L 220,94.5 284,31.5 346,0 h 95 l 63,31.5 63,63"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,203.52,0)"
-//                 id="path7"
-//               />
-//               <path
-//                 d="M 252,662 V 126 L 284,31.5 346,0 h 64 M 158,441 h 220"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,239.808,0)"
-//                 id="path8"
-//               />
-//               <path
-//                 d="M 252,662 V 126 L 284,31.5 346,0 h 64 M 158,441 h 220"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,264,0)"
-//                 id="path9"
-//               />
-//               <path
-//                 d="m 189,662 31,-32 32,32 -32,31 -31,-31 M 220,441 V 0"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,288.192,0)"
-//                 id="path10"
-//               />
-//               <path
-//                 d="m 536,346 -32,64 -94,31 h -95 l -95,-31 -31,-64 31,-62 64,-32 157,-32 63,-31 32,-63 V 94.5 L 504,31.5 410,0 h -95 l -95,31.5 -31,63"
-//                 style={{ strokeWidth: "0.04306in" }}
-//                 transform="matrix(0.064,0,0,-0.064,304.32,0)"
-//                 id="path11"
-//               />
-//             </g>
-//           </g>
-//         </g>
-//       </svg>
-//     );
-//   }
-  
-export default function Logo() {
-    return (
-      <svg
-        width="210mm"
-        height="56mm"
-        viewBox="0 0 210 56"
-        version="1.1"
-        id="svg1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
+
+import React from "react";
+
+const Logo = () => (
+  <svg
+    width="210mm"
+    height="56mm"
+    viewBox="0 0 210 56"
+    version="1.1"
+    id="svg1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <defs>
+      <rect x="103.25089" y="29.039314" width="535.61402" height="116.15726" id="rect2" />
+      <rect x="75.290955" y="173.39735" width="574.9491" height="269.2222" id="rect1" />
+    </defs>
+    <g className='logo' id="layer1">
+      <g 
+        style={{
+          fill: "none",
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+        }}
+        transform="scale(0.264583, 0.264583)"
       >
-        <defs id="defs1" />
-        <g  className='logo' id="layer1">
+        <g id="logo-content" transform="translate(103.25, 90.9338)">
           <path
-            className='logo'
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 633.93815,84.149799 -5.824,6.784 h -40.384 l -5.76,-6.592 h 36.032 l 2.368,-2.752 v -9.792 h -36.672 v -18.56 l 6.08,-7.104 h 45.696 l -5.632,6.592 h -29.888 l -2.88,3.264 v 9.216 h 36.864 z"
-            transform="scale(0.264583)"
-            id="path18"
+            d="M 118 700 h 540 v -350 q 0 -95 -47 -175.5 t -127.5 -127.5 t -175.5 -47 h -291 v 182 h 291 q 70 0 119 49 t 49 119 v 168 h -358 v 182 z"
+            transform="scale(0.064, -0.064)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 563.02616,46.133799 h 13.44 v 44.8 h -13.44 z"
-            transform="scale(0.264583)"
-            id="path17"
+            d="M 747 71 q -44 -43 -99 -57 t -129 -14 q -14 0 -43 1 h -46 q -50 0 -77 1 t -76 7.5 t -77.5 19 t -65.5 38 t -57 62 t -34.5 93 t -14.5 129.5 q 0 227 137 306 q 22 12 48.5 21 t 49 13.5 t 56 6.5 t 53.5 2.5 t 58 0.5 q 50 0 77 -1 t 76 -7.5 t 77.5 -19 t 65.5 -38 t 57 -62 t 34.5 -93 t 14.5 -129.5 q 0 -196 -85 -280 z M 651 351 q 0 37 -4 62.5 t -8 44.5 t -19.5 30 t -26.5 18 t -42 9.5 t -52 3 t -69 0.5 q -47 0 -71.5 -0.5 t -55.5 -5 t -48 -14.5 q -45 -26 -45 -148 q 0 -29 2 -50 t 4.5 -38.5 t 11.5 -29.5 t 14.5 -21 t 23 -14.5 t 28 -8.5 t 37.5 -4.5 t 44 -1.5 h 55 q 17 0 49 -1 h 59 t 49.5 4.5 t 31.5 13.5 q 32 31 32 151 z"
+            transform="matrix(0.064 0 0 -0.064 44.992 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 553.4901,52.725799 h -16.576 v 38.208 h -13.376 v -38.208 h -16.768 l -5.696,-6.592 h 58.112 z"
-            transform="scale(0.264583)"
-            id="path16"
+            d="M 632 0 v 257 h -390 v -257 h -182 v 700 h 182 v -282 h 390 v 282 h 182 v -700 h -182 z"
+            transform="matrix(0.064 0 0 -0.064 100.096 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 495.37804,52.725799 h -16.576 v 38.208 h -13.376 v -38.208 h -16.768 l -5.696,-6.592 h 58.112 z"
-            transform="scale(0.264583)"
-            id="path15"
+            d="M 414 700 q 101 0 186.5 -50 t 135.5 -135.5 t 50 -186.5 v -328 h -182 v 328 q 0 79 -55.5 134.5 t -134.5 55.5 t -134.5 -55.5 t -55.5 -134.5 v -328 h -182 v 328 q 0 76 29.5 144.5 t 79.5 118.5 t 118.5 79.5 t 144.5 29.5 z"
+            transform="matrix(0.064 0 0 -0.064 156.032 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 435.47405,84.341799 5.76,6.592 h -50.176 v -44.8 h 50.304 l -5.696,6.592 h -31.168 v 12.544 h 33.856 v 6.592 h -33.856 v 12.48 z"
-            transform="scale(0.264583)"
-            id="path14"
+            d="M 719 700 v -182 h -263 v -518 h -182 v 518 h -262 v 182 h 707 z"
+            transform="matrix(0.064 0 0 -0.064 235.328 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 382.80199,52.725799 h -16.576 v 38.208 h -13.376 v -38.208 h -16.768 l -5.696,-6.592 h 58.112 z"
-            transform="scale(0.264583)"
-            id="path13"
+            d="M 378 518 q -47 0 -86.5 -23.5 t -61.5 -63.5 h 482 v -162 h -482 q 15 -26 37 -45.5 t 51 -30.5 t 60 -11 h 392 v -182 h -392 q -95 0 -175.5 47 t -127.5 127.5 t -47 175.5 t 47 175.5 t 127.5 127.5 t 175.5 47 h 392 v -182 h -392 z"
+            transform="matrix(0.064 0 0 -0.064 282.112 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 308.62607,90.933799 -36.672,-24.384 v 24.384 h -13.376 v -44.8 l 36.864,24.192 v -24.192 h 13.184 z"
-            transform="scale(0.264583)"
-            id="path12"
+            d="M 712 518 h -262 v -210 q 0 -34 16.5 -63 t 46 -46 t 63.5 -17 h 136 v -182 h -136 q -128 0 -218 90.5 t -90 217.5 v 210 h -256 v 182 h 700 v -182 z"
+            transform="matrix(0.064 0 0 -0.064 334.208 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 253.45806,46.133799 v 44.8 h -13.504 v -19.136 h -23.168 v 19.136 h -13.312 v -44.8 h 13.312 l 0.28519,18.786807 22.88281,0.285193 v -19.072 z"
-            id="path11"
-            transform="scale(0.264583)"
+            d="M 712 518 h -262 v -210 q 0 -34 16.5 -63 t 46 -46 t 63.5 -17 h 136 v -182 h -136 q -128 0 -218 90.5 t -90 217.5 v 210 h -256 v 182 h 700 v -182 z"
+            transform="matrix(0.064 0 0 -0.064 382.208 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 198.35403,90.933799 v -44.8 h -50.816 v 32.448 l 10.88,12.352 z m -13.376,-38.208 v 31.616 h -16.448 l -7.616,-8.64 v -22.976 z"
-            transform="scale(0.264583)"
-            id="path10"
+            d="M 242 0 h -182 v 700 h 182 v -700 z"
+            transform="matrix(0.064 0 0 -0.064 430.208 0)"
           />
           <path
-            style={{
-              fontSize: "64px",
-              fontFamily: "'ROG Fonts'",
-              whiteSpace: "pre",
-              stroke: "#374bff",
-              strokeWidth: 2.64567,
-              strokeDasharray: "none",
-              strokeOpacity: 0.608808,
-            }}
-            d="m 110.29,90.933799 -5.76,-6.592 h 24.64 v -38.208 h 13.248 v 44.8 z"
-            transform="scale(0.264583)"
-            id="text1"
+            d="M 594 431 q 58 0 107.5 -29 t 78.5 -78.5 t 29 -108.5 q 0 -89 -63 -152 t -152 -63 h -527 v 182 h 517 q 18 0 30.5 12.5 t 12.5 31 t -12.5 31 t -30.5 12.5 h -335 q -89 0 -152 63 t -63 152.5 t 63 152.5 t 152 63 h 527 v -182 h -517 q -18 0 -30.5 -12.5 t -12.5 -31 t 12.5 -31 t 30.5 -12.5 h 335 z"
+            transform="matrix(0.064 0 0 -0.064 449.536 0)"
           />
         </g>
-      </svg>
-    );
-  }
+      </g>
+    </g>
+  </svg>
+);
+
+export default Logo;
+
+  
+  
   
