88a3f1e8460b3352fc8e65eaae4581af8250e211
John Tettis <johndavidtettis@gmail.com>
2024-12-07 19:23:49 -0500

    created loading screen component

---
+++ src/Components/Utils/LoadingScreen.js
@@ -0,0 +1,18 @@
+import Logo from './Logo';
+import React, { useEffect, useRef } from 'react';
+import '../../styles/LoadingScreen.css'
+
+const LoadingScreen= ({loading})=>{
+
+   const loadClass = 'loading-screen' + (loading ? '':' move-up');
+    return(
+    <div className={loadClass}>
+        <Logo/>
+    </div>
+        
+
+    )
+
+}
+
+export default LoadingScreen
