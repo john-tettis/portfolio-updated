676c8acb3c5dd031573b5dfe1b78f5cf4d50d2f5
John Tettis <johndavidtettis@gmail.com>
2024-12-08 23:06:18 -0500

    updated classNames to dynamically trigger animations

--- src/Components/ScrollSection.js
+++ src/Components/ScrollSection.js
@@ -3,11 +3,12 @@ import About from './About';
 import Experience from './Experience';
 import Projects from './Projects';
 import '../styles/ScrollSection.css'
+import '../styles/Utils/Animations.css'
  
-const ScrollSection= forwardRef(({navRefs},ref)=>{
+const ScrollSection= forwardRef(({navRefs,loading},ref)=>{
     const {about,experience,projects} = navRefs
     return(
-    <div ref={ref}className='scroll-section fade-in'>
+    <div ref={ref}className={'scroll-section ' + (loading ? '':'fade-in')}>
         <About ref ={about}/>
         <Experience ref={experience}/>
         <Projects ref={projects}/>
