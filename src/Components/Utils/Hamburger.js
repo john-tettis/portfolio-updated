2d906bb95664b6d406a0cf73397a25c43ab79672
John Tettis <johndavidtettis@gmail.com>
2024-12-09 21:43:27 -0500

    added toggle menu function!

--- src/Components/Utils/Hamburger.js
+++ src/Components/Utils/Hamburger.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../../styles/Hamburger.css'
 
-const Hamburger= ({id})=>{
+const Hamburger= ({toggleMenu})=>{
 
     return(
         <>
-            <input type="checkbox" id="hamburger"/>
+            <input type="checkbox" onClick={toggleMenu} id="hamburger"/>
             <label htmlFor="hamburger" className="toggle">
                 <div className="bars" id="bar1"></div>
                 <div className="bars" id="bar2"></div>
