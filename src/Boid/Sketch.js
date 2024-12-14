9d3ddb6fdad5f6494fa860dfc8303acce7905044
johntettis <johndavidtettis@gmail.com>
2024-12-14 15:48:27 -0500

    added logic to delay flocking behavior afer mouse is released, to rescatter boids after they converge on mouse.

--- src/Boid/Sketch.js
+++ src/Boid/Sketch.js
@@ -5,6 +5,10 @@
 import p5 from 'p5'
 import Boid from './boid'
 const flock = [];
+//ms for minimum click time to activate the delay / scatter
+const mouseReleaseDuration = 100
+let mouseDuration = 0;
+let mouseReleased = false;
 
 function setup(p,canvasRef) {
   const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
@@ -17,38 +21,41 @@ function setup(p,canvasRef) {
 }
 
 function draw(p) {
+  // console.log({mouseReleased, isMousePressed})
+  if(p.mouseIsPressed) mouseDuration++;
   p.background(p.color(12, 33, 47));
   for (let boid of flock) {
+    if(mouseReleased === true){
+      
+      boid.setDelay(100);
+    }
+   
     boid.edges(p);
-    boid.flock(p,flock);
+    boid.flock(p,flock,p.mouseIsPressed);
     boid.update(p);
     boid.show(p);
   }
-
-//   let colors = [];
-//   colorMode(HSB, 255);
-//   for (let i = 0; i < flock.length; i += 3) {
-//     let r = map(flock[i].velocity.mag(), 0, 5, 0, 255);
-//     let g = map(flock[i + 1].velocity.mag(), 0, 5, 0, 255);
-//     let b = map(flock[i + 2].velocity.mag(), 0, 5, 0, 255);
-//     colors.push(color(r, g, b));
-//   }
-
-//   let w = width / colors.length;
-//   let h = 100;
-//   colors.sort((a, b) => hue(a) - hue(b));
-//   for (let i = 0; i < colors.length; i++) {
-//     let c = colors[i];
-//     fill(c);
-//     noStroke();
-//     rect(i * w, height - h, w, h);
-//   }
+  mouseReleased = false;
+  
 }
+
+// Function to handle mouse release
+
 
 export default function Sketch(ref){
     return  (p) => {
+      
         p.setup = () => setup(p, ref);
         p.draw = () => draw(p);
+        p.mouseReleased= ()=> {
+          if(mouseDuration >  mouseReleaseDuration){
+            mouseReleased = true
+            mouseDuration = 0;
+            console.log('MOUSE RELEASE')
+          }
+         
+        }
+        
       };
 
 }
