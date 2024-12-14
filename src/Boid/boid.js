0eb225b9e8f219a47cdd1a74440bb7f2e5835510
johntettis <johndavidtettis@gmail.com>
2024-12-14 14:05:03 -0500

    added mouse attraction

--- src/Boid/boid.js
+++ src/Boid/boid.js
@@ -6,11 +6,13 @@ class Boid {
     this.velocity = p5.Vector.random2D(); // Use p5 for global methods
     this.velocity.setMag(p.random(2, 4));
     this.acceleration = p.createVector();
+    this.color = p.color(217, 133, 60);
     this.maxForce = 0.2;
     this.maxSpeed = 5;
     this.alignVal =1;
-    this.cohesionVal =1;
-    this.separationVal = 1;
+    this.cohesionVal =1.3;
+    this.separationVal = 1.2;
+    this.mouseVal = 1.5
   }
 
   edges(p) {
@@ -88,20 +90,44 @@ class Boid {
     }
     return steering;
   }
-
+  mouseAttraction(p) {
+    let mousePos = p.createVector(p.mouseX, p.mouseY); // Get mouse position
+    let desired = p5.Vector.sub(mousePos, this.position); // Vector pointing from boid to mouse
+    let distance = desired.mag(); // Distance from the boid to the mouse
+  
+    // Set a perception radius for mouse attraction (similar to the other behaviors)
+    let perceptionRadius = 100; // You can adjust this value to control the range
+  
+    // If the mouse is within the perception radius, apply the attraction force
+    if (distance < perceptionRadius) {
+      desired.setMag(this.maxSpeed); // Set the desired speed towards the mouse
+      let steer = p5.Vector.sub(desired, this.velocity); // Calculate steering force
+      steer.limit(this.maxForce); // Limit the force applied
+  
+      return steer;
+    } else {
+      // No attraction force if outside the perception radius
+      return p.createVector(0, 0);
+    }
+  }
+  
   flock(p, boids) {
     let alignment = this.align(p, boids);
     let cohesion = this.cohesion(p, boids);
     let separation = this.separation(p, boids);
-
+    let mouseForce = this.mouseAttraction(p); // Get the force toward the mouse
+  
     alignment.mult(this.alignVal);
     cohesion.mult(this.cohesionVal);
     separation.mult(this.separationVal);
-
+    mouseForce.mult(this.mouseVal)
+  
     this.acceleration.add(alignment);
     this.acceleration.add(cohesion);
     this.acceleration.add(separation);
+    this.acceleration.add(mouseForce); // Add mouse attraction force
   }
+  
 
   update() {
     this.position.add(this.velocity);
@@ -111,8 +137,8 @@ class Boid {
   }
 
   show(p) {
-    p.strokeWeight(6);
-    p.stroke(255);
+    p.strokeWeight(10);
+    p.stroke(this.color);
     p.point(this.position.x, this.position.y);
   }
 }
