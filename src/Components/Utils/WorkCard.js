e142d8882901647245713b2d04dd94cb9e314b28
John Tettis <johndavidtettis@gmail.com>
2025-07-12 15:04:55 -0400

    updated to use nda tooltip

--- src/Components/Utils/WorkCard.js
+++ src/Components/Utils/WorkCard.js
@@ -1,37 +1,38 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import SkillBubble from './SkillBubble'
 import '../../styles/WorkCard.css'
+import NDABubble from './NDABubble'
 
-const WorkCard= ({title, company, description, start,end,skills})=>{
 
-
-    return(
-    <div className='card'>
-        <div className="card-row-top">
-            <h4 className="duration card-column-left">{start} {'\u2014'} {end}</h4>
-            <div className=' card-column-right'>
-                <h4 className='card-title'>{title}</h4>
-                <p className="card-company">{company}</p>
+const WorkCard = ({ title, company, description, start, end, skills,NDA =false}) => {
+  
+    return (
+        <div className='card'>
+            <div className="card-row-top">
+                <h4 className="duration card-column-left">{start} {'\u2014'} {end}</h4>
+                <div className='card-column-right'>
+                    <h4 className='card-title'>{title}</h4>
+                    <p className="card-company">
+                        {company}
+                        {NDA && (
+                           <NDABubble />
+                        )}
+                    </p>
+                </div>
+            </div>
+            <div className="card-row-bottom">
+                <div className="card-skills card-column-left">
+                    {skills.map((skill, index) => (
+                        <SkillBubble
+                            key={index}
+                            skill={skill}
+                        />
+                    ))}
+                </div>
+                <p className="card-description card-column-right">{description}</p>
             </div>
         </div>
-         <div className="card-row-bottom">
-         <div className="card-skills card-column-left">
-                {skills.map((skill, index) => (
-                    <SkillBubble 
-                        key={index} 
-                        skill={skill}
-                    />
-                ))}
-            </div>
-            <p className="card-description card-column-right">{description}</p>
-            
-
-        </div>
-    </div>
-        
-
     )
-
 }
 
 export default WorkCard
